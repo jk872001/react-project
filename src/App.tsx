e8a32ce37fcd694585be322be55e11d15fce6e6a
jk872001 <72179040+jk872001@users.noreply.github.com>
2024-10-22 19:54:18 +0530

    code initialise

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+// import { Button } from 'shadcn-ui';
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline">
+      Shadcn Ui
+    </h1>
+   
+    </>
+  )
+}
+
+export default App
